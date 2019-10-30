7c174ccffd6c54aeec401e922d361bfb81f7b023
Pol-byte <56954677+Pol-byte@users.noreply.github.com>
2019-10-30 10:52:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let name=document.getElementById("name");
+name.addEventListener("input", function (event) {
+    if (!name.validity.patternMismatch) {
+      name.setCustomValidity("");
+    } else {
+      name.setCustomValidity("Имя должно начинаться с большой буквы, состоять из букв латинского или русского алфавита и содержать в себе только буквы");
+    }
+  });
+let secname=document.getElementById("secname");
+secname.addEventListener("input", function (event) {
+    if (!secname.validity.patternMismatch) {
+        secname.setCustomValidity("");
+    } else {
+        secname.setCustomValidity("Фамилия должна начинаться с большой буквы, состоять из букв латинского или русского алфавита и содержать в себе только буквы");
+    }
+  });
+let surname=document.getElementById("secname");
+surname.addEventListener("input", function (event) {
+    if (!fathername.validity.patternMismatch) {
+        surname.setCustomValidity("");
+    } else {
+        surname.setCustomValidity("Отчество должно начинаться с большой буквы, состоять из букв латинского или русского алфавита и содержать в себе только буквы");
+    }
+  });
+  let password=document.getElementById("password");
+    password.addEventListener("input", function (event){
+        if (!password.validity.patternMismatch) {
+                password.setCustomValidity("");
+        } else {
+                password.setCustomValidity("Пароль должен содержать цифры, символы нижнего и верхнего регистра.");
+            }
+        });
+        document.getElementById("form").onsubmit = function (evt) {
+            localStorage.clear();
+            evt.preventDefault();
+            console.log(evt);
+            let object = {};
+            for (let i = 0; i < 7; i++) {
+                object[evt.target[i].name] = evt.target[i].value;
+            }
+        localStorage.setItem("form", JSON.stringify(object));
+        }
+        window.onload = function(){
+            if(localStorage.getItem("form")){
+                let object = JSON.parse(localStorage.getItem("form"));
+                let arr = ["name","secname","surname","email","password","date","sex"];
+                arr.forEach(element => {
+                    if(document.getElementById(element))
+                    document.getElementById(element).value = object[element];
+                });
+            }
+        } 
